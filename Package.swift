5c396ce861679046b47ed7955cac531ff338caac
osls <balipastika@gmail.com>
2023-01-03 18:57:49 +0800

    remove test

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.5
+// swift-/Users/osls/Desktop/DataSourcePackage/Package.swifttools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DataSourcePackage",
+    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -22,9 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DataSourcePackage",
-            dependencies: []),
-        .testTarget(
-            name: "DataSourcePackageTests",
-            dependencies: ["DataSourcePackage"]),
+            dependencies: ["Alamofire", "Realm", "RxSwift"]),
     ]
 )
